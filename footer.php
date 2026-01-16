e13da4e9f8f902b6fdfa933566f092bfe14bf445
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 14:44:37 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-    </main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Website. All Rights Reserved.</p>
-    </footer>
+</main>
+
+<footer class="site-footer">
+    <p>© <?php echo date("Y"); ?> My PHP Website. All Rights Reserved.</p>
+</footer>
+
 </body>
 </html>
-
