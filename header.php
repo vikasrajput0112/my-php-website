6ecb5d0c86e80896019e0e263f6c03a37d8c577c
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 15:32:14 +0530

    Update header.php

--- header.php
+++ header.php
@@ -3,18 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>My Website</title>
-    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<header class="site-header">
-    <div class="container">
-        <div class="logo">
-            My<span>Website</span>
-        </div>
-
-        <nav class="navbar">
+<header class="header">
+    <div class="container header-flex">
+        <div class="logo">My<span>Website</span></div>
+        <nav class="nav">
             <a href="index.php">Home</a>
             <a href="about.php">About</a>
             <a href="service.php">Services</a>
@@ -23,4 +20,4 @@
     </div>
 </header>
 
-<main class="content">
+<main>
