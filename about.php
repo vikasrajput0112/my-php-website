c80e81baae454851366f3ec0c2bc6cc83f8ca7e0
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 14:33:07 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
 <h2>About Us</h2>
-<p>We are a company that provides amazing services to our clients.</p>
+<p>We are a company that provides amazing services to our clients and u can also try.</p>
 <?php include 'footer.php'; ?>
 
