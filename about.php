bfc4118755d696c49cdce0f7659b7fcd26193cf6
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 14:55:27 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,5 +1,9 @@
 <?php include 'header.php'; ?>
-<h2>About Us</h2>
-<p>We are a company that provides amazing services to our clients and u can also try.</p>
-<?php include 'footer.php'; ?>
 
+<h2>About Us</h2>
+<p>
+    We are a technology-driven team focused on building reliable
+    and scalable digital solutions for modern businesses.
+</p>
+
+<?php include 'footer.php'; ?>
