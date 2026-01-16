0ff8cef53bf2c0e06d36a58916f449082ceac5e4
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 14:56:13 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,13 +1,7 @@
 <?php include 'header.php'; ?>
-<h2>Contact Us</h2>
-<form action="process_form.php" method="post">
-    <label>Name:</label><br>
-    <input type="text" name="name" required><br>
-    <label>Email:</label><br>
-    <input type="email" name="email" required><br>
-    <label>Message:</label><br>
-    <textarea name="message" required></textarea><br>
-    <button type="submit">Send</button>
-</form>
-<?php include 'footer.php'; ?>
 
+<h2>Contact Us</h2>
+<p>Email: contact@mywebsite.com</p>
+<p>Phone: +91-1234567890</p>
+
+<?php include 'footer.php'; ?>
