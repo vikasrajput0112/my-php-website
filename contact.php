eea7490e6bf25b5f00d50098ce5366df57d757bb
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 16:02:03 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,13 +3,9 @@
 <section class="section alt">
     <div class="container content">
         <h2>Contact Us</h2>
-        <p>
-            Have a project in mind or need expert guidance?
-            Reach out to us and we’ll be happy to help.
-        </p>
-        <p><strong>Email:</strong> contact@mywebsite.com</p>
-        <p><strong>Phone:</strong> +91-X123456XX</p>
-        <p><strong>Location:</strong> India</p>
+        <p>Email: contact@mywebsite.com</p>
+        <p>Phone: +91-XXXXXXXXXX</p>
+        <p>Location: India</p>
     </div>
 </section>
 
